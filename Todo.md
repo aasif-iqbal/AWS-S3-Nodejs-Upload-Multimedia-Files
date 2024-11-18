0a40ad07334922945f8929e15c5f4aa5d42d2797
aasif-iqbal <aasif.iqbal3000@gmail.com>
2024-11-19 02:57:24 +0530

    add image upload func

--- Todo.md
+++ Todo.md
@@ -1,4 +1,7 @@
-1. Post a photo with multipart/form-data
-2. Use Multer to upload image 
-3. Use memoryStorage to store image as buffer.(don't store image on disk)
-4. create IAM user. add policy for s3 bucket - Read[Get] - Write[Put, Delete]
+1. Post a photo with multipart/form-data [✓]
+2. Use Multer to upload image [✓]
+3. Use memoryStorage to store image as buffer.(don't store image on disk) [✓]
+4. create IAM user. add policy for s3 bucket - Read[Get] - Write[Put, Delete] 
+
+
+[✓] - Done 
--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import path from 'path';
+
+import multer from 'multer'
+import sharp from 'sharp'
+import crypto from 'crypto'
+
 const app = express();
 
 // Set the view engine to EJS
@@ -11,9 +16,26 @@ app.set('views', path.resolve('src/views'));
 // Middleware for serving static files
 app.use(express.static(path.resolve('src/public')));
 
+// Middleware for parsing form data
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
 app.get('/', (req, res) => {
   const data = { title: 'Welcome to My App', message: 'Hello, EJS with ES6!' };
   res.render('index', data);
 });
 
+
+app.post('/posts', upload.single('image'), async (req, res) => {
+    const file = req.file;
+    console.log(file);
+    const filename = `${crypto.createHash('sha256').update(file.originalname).digest('hex')}.jpg`;
+    console.log(filename);
+    
+    const resizedImage = await sharp(file.buffer).resize(400).toBuffer();
+    
+    await sharp(resizedImage).toFile(`src/public/images/${filename}`);
+    
+    res.send(`Image uploaded successfully! The resized image is available at /images/${filename}`); 
+})
 export default app;
